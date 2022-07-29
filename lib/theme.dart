3850ae4dc89f3aae55524bd74fd7d7d655df611d
Ben Siu <bensiu123@yahoo.com.hk>
2022-07-30 01:02:33 +0800

    formatted

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,7 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'constants.dart';
-import 'package:flutter/material.dart';
 
 final ThemeData theme = ThemeData(
   scaffoldBackgroundColor: Colors.white,
